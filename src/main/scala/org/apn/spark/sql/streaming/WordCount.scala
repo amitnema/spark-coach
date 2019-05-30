804d95faabbe3f7ec6b23f02d958770774d7024c
Amit Nema <amitnema@users.noreply.github.com>
2019-05-30 11:57:57 +0530

    Structured stream example updated

--- src/main/scala/org/apn/spark/sql/streaming/WordCount.scala
+++ src/main/scala/org/apn/spark/sql/streaming/WordCount.scala
@@ -1,13 +1,15 @@
 package org.apn.spark.sql.streaming
 
-import java.io.PrintWriter
 import java.util.UUID
 
 import org.apache.commons.lang3.StringUtils
+import org.apache.spark.sql.streaming.OutputMode.{Append, Complete}
 import org.apache.spark.sql.{Dataset, SparkSession}
 
-import scala.collection.mutable.ListBuffer
-
+/**
+  *
+  * @author Amit Nema
+  */
 object WordCount {
 
   type WordCountHandler = (Dataset[ WordCount ], Long) => Unit
@@ -25,52 +27,47 @@ object WordCount {
 
     val spark = SparkSession
       .builder
-      .appName ( "StructuredKafkaWordCount" )
+      .appName ( getClass.getName )
       .getOrCreate ( )
 
-    var results = ListBuffer.empty [ Array[ WordCount ] ]
+    countWordConsole ( spark, bootstrapServers, subscribeType, topics, checkpointLocation )
 
-    countWordKafka ( spark, bootstrapServers, subscribeType, topics, checkpointLocation ) { (wordsCount: Dataset[ WordCount ], _: Long) =>
-      results += wordsCount.collect ( )
-      wordsCount.foreach ( x => println ( x.word, x.wc ) )
-    }
+    /*countWordBatch ( spark, bootstrapServers, subscribeType, topics, checkpointLocation ) {
+      (wordsCount: Dataset[ WordCount ], _: Long) =>
+        wordsCount.foreach ( x => println ( x.word, x.wc ) )
+    }*/
 
-    // Create DataSet representing the stream of input lines from kafka
-   /* val lines = spark
-      .readStream
-      .format ( "kafka" )
-      .option ( "kafka.bootstrap.servers", bootstrapServers )
-      .option ( subscribeType, topics )
-      .load ( )
-      .selectExpr ( "CAST(value AS STRING)" )
-      .as [ String ]
+    //    countWordSave ( spark, bootstrapServers, subscribeType, topics, checkpointLocation, "spark-data" )
+  }
 
-    lines.createOrReplaceTempView ( "stream_dataset" )
-    val delim = StringUtils.SPACE
-    val wordCounts = spark.sql ( s"SELECT word, count(1) AS wc FROM (select explode(split(value, '$delim')) as word from stream_dataset) group by word order by word" )
+  def countWordConsole(spark: SparkSession, bootstrapServers: String, subscribeType: String, topics: String, checkpointLocation: String, delim: String = StringUtils.SPACE): Unit = {
 
-    // Start running the query that prints the running counts to the console
-    val query = wordCounts.writeStream
-      .outputMode ( "complete" )
-      .format ( "console" )
+    // Start running the query that sends the running counts to the handler
+    val query = countWord ( spark, bootstrapServers, subscribeType, topics, delim ).writeStream
+      .outputMode ( Complete ( ) )
       .option ( "checkpointLocation", checkpointLocation )
+      .format ( "console" )
       .start ( )
 
-    query.awaitTermination ( )*/
+    query.awaitTermination ( )
   }
 
-  def countWordSave(spark: SparkSession, pathOut: String, bootstrapServers: String, subscribeType: String, topics: String, checkpointLocation: String, delim: String = StringUtils.SPACE)(handler: WordCountHandler): Unit = {
-    var results = ListBuffer.empty [ Array[ WordCount ] ]
-    countWordKafka ( spark, bootstrapServers, subscribeType, topics, checkpointLocation, delim ) { (wordsCount: Dataset[ WordCount ], _: Long) =>
-      results += wordsCount.collect ( )
-    }
-    new PrintWriter ( pathOut ) {
-      write ( results.mkString )
-      close ( )
-    }
+  def countWordBatch(spark: SparkSession, bootstrapServers: String, subscribeType: String, topics: String, checkpointLocation: String, delim: String = StringUtils.SPACE)(handler: WordCountHandler): Unit = {
+    import spark.implicits._
+
+    // Start running the query that sends the running counts to the handler
+    val query = countWord ( spark, bootstrapServers, subscribeType, topics, delim ).writeStream
+      .outputMode ( Complete ( ) )
+      .option ( "checkpointLocation", checkpointLocation )
+      .foreachBatch ( (batchDF: Dataset[ WordCount ], batchId: Long) => {
+        handler ( batchDF.sort ( $"word" ), batchId )
+      } )
+      .start ( )
+
+    query.awaitTermination ( )
   }
 
-  def countWordKafka(spark: SparkSession, bootstrapServers: String, subscribeType: String, topics: String, checkpointLocation: String, delim: String = StringUtils.SPACE)(handler: WordCountHandler): Unit = {
+  private def countWord(spark: SparkSession, bootstrapServers: String, subscribeType: String, topics: String, delim: String = StringUtils.SPACE) = {
     import spark.implicits._
 
     // Create DataSet representing the stream of input lines from kafka
@@ -80,33 +77,27 @@ object WordCount {
       .option ( "kafka.bootstrap.servers", bootstrapServers )
       .option ( subscribeType, topics )
       .load ( )
-      .selectExpr ( "CAST(value AS STRING)" )
-      .as [ String ]
+      .selectExpr ( "CAST(value AS STRING)", "CAST(timestamp AS timestamp)" )
+      //.withWatermark("timestamp", "30 seconds")
+      .as [ (String, String) ]
 
     lines.createOrReplaceTempView ( "stream_dataset" )
-    val wordCounts = spark.sql ( s"SELECT word, count(1) AS wc FROM (select explode(split(value, '$delim')) as word from stream_dataset) group by word order by word" ).as[WordCount]
+    spark.sql ( s"SELECT word, count(1) AS wc FROM (select explode(split(value, '$delim')) as word from stream_dataset) group by word order by word" ).as [ WordCount ]
+  }
 
-    // Start running the query that prints the running counts to the console
-    /*val query = wordCounts.writeStream
-      .outputMode ( "complete" )
-      .format ( "console" )
+  /**
+    * @deprecated Not Working
+    */
+  def countWordSave(spark: SparkSession, bootstrapServers: String, subscribeType: String, topics: String, checkpointLocation: String, pathOut: String, delim: String = StringUtils.SPACE): Unit = {
+    // Start running the query that sends the running counts to the handler
+    val query = countWord ( spark, bootstrapServers, subscribeType, topics, delim ).writeStream
+      .outputMode ( Append )
       .option ( "checkpointLocation", checkpointLocation )
-      .start ( )*/
-
-
-    // Start running the query that prints the running counts to the console
-    val query = wordCounts.writeStream
-            .outputMode ( "complete" )
-      //      .format ( "console" )
-      .option ( "checkpointLocation", checkpointLocation )
-      .foreachBatch ( (batchDF: Dataset[ WordCount ], batchId: Long) => {
-        handler ( batchDF.sort ( $"word" ), batchId )
-      } )
+      .format ( "json" ) // can be "orc", "json", "csv", etc.
+      .option ( "path", pathOut )
       .start ( )
 
     query.awaitTermination ( )
-
-
   }
 }
 
